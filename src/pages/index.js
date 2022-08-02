af44bc38e034d1e30cbcb0aebdf853ae87caa556
Douglas Byfield <douglasbyfield@yahoo.com>
2022-08-02 15:56:24 -0500

    edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => {
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I have no idea what im doing tbh but im trying a ting.</p>
     </main>
   );
 };
